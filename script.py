a039facbf586f4fd1f6f95d8b14bca94ce5d0805
Garrett Gordon <gagordon@mit.edu>
2024-10-18 21:10:45 -0400

    get return function

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ markets = {
     "Will Donald Trump win the presidency?": ["PRES-2024-DJT", "0xdd22472e552920b8438158ea7238bfadfa4f736aa4cee91a6b86c39ead110917"]
 }
 
-def get_return(yes_contracts : float, yes_price : float, no_contracts: float, no_price : float) -> (float,float):
+def get_return(yes_contracts : float, yes_price : float, no_contracts: float, no_price : float) -> tuple[float,float]:
     investment = yes_contracts*yes_price + no_contracts*no_price
     return (yes_contracts / investment, no_contracts / investment)
 
